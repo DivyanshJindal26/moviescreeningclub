d82b04db8fbb47e0dd87867bd4c29172ccc4bfb5
DivyanshJindal26 <124961518+DivyanshJindal26@users.noreply.github.com>
2026-04-02 16:04:47 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -9,14 +9,3 @@ export const api = axios.create({
   baseURL: baseUrl,
   withCredentials: true
 })
-
-// ✅ ADD THIS
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token') // or wherever you store it
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
-  }
-
-  return config
-})
